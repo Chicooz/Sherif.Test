5034d869ef15428a8f62e90fa0510fb6bbf33f8a
Sherif Nnegm <sherif@sherifnegm.com>
2012-12-15 18:15:59 +0200

    title change

--- register-step2.php
+++ register-step2.php
@@ -2,7 +2,7 @@
 <html>
 <head>
 <meta charset="utf-8">
-<title>etwazf jobs - Finding Jobs For You</title>
+<title>Test Blah</title>
 <link rel="stylesheet" type="text/css" href="css/reset.css">
 <link rel="stylesheet" type="text/css" href="css/style.css">
 <link rel="stylesheet" type="text/css" href="css/tab.css">
